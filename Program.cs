a78de1bc4c5287349324f2b45043adeda75dfcca
emandere <emandere@gmail.com>
2020-01-06 02:45:34 -0500

    Added Mongodb connection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using AutoMapper;
+using forex_import.Models;
 
 namespace forex_import
 {
@@ -6,6 +11,20 @@ namespace forex_import
     {
         static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            var serviceProvider = new ServiceCollection()
+                .AddAutoMapper()
+                .Configure<Settings>(options =>
+                {
+                    options.ConnectionString 
+                        = configuration.GetSection("MongoConnection:ConnectionString").Value;
+                    options.Database 
+                        = configuration.GetSection("MongoConnection:Database").Value;
+                })
+                .BuildServiceProvider(); 
             Console.WriteLine("Hello World!");
         }
     }
