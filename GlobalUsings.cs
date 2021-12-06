4b8ff3fb8a781c10b9773afd312086faa9535b28
emandere <emandere@gmail.com>
2021-12-06 00:16:53 -0500

    Messing with 6.0

---
+++ GlobalUsings.cs
@@ -0,0 +1,14 @@
+global using System;
+global using System.Linq;
+global using System.IO;
+global using System.Threading.Tasks;
+global using System.Net.Http;
+global using System.Text;
+global using System.Text.Json;
+global using System.Collections.Generic;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+
+global using AutoMapper;
+global using forex_import.Models;
+global using forex_import.Config;
